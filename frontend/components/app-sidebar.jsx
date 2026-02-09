b8cd2fa5f98d2a532936acb72125d24e7f4d47f5
Guaracy Mendes <lepton30@gmail.com>
2026-02-09 19:15:36 -0400

    frontend: Initial commit

---
+++ frontend/components/app-sidebar.jsx
@@ -0,0 +1,150 @@
+"use client"
+
+import * as React from "react"
+import {
+  IconCamera,
+  IconCreditCard,
+  IconDashboard,
+  IconFileAi,
+  IconFileDescription,
+  IconListDetails,
+  IconPackage,
+  IconReport,
+  IconSettings,
+  IconShoppingCart,
+} from "@tabler/icons-react"
+
+import { NavDocuments } from "@/components/nav-documents"
+import { NavMain } from "@/components/nav-main"
+import { NavSecondary } from "@/components/nav-secondary"
+import { NavUser } from "@/components/nav-user"
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar"
+
+const data = {
+  user: {
+    name: "admin",
+    email: "admin@email.com",
+    avatar: "/public/images/pos.png",
+  },
+  navMain: [
+    {
+      title: "Dashboard",
+      url: "#",
+      icon: IconDashboard,
+    },
+    {
+      title: "Categories",
+      url: "#",
+      icon: IconListDetails,
+    },
+    {
+      title: "Products",
+      url: "#",
+      icon: IconPackage,
+    },
+    {
+      title: "Sales",
+      url: "#",
+      icon: IconShoppingCart,
+    },
+  ],
+  navClouds: [
+    {
+      title: "Capture",
+      icon: IconCamera,
+      isActive: true,
+      url: "#",
+      items: [
+        {
+          title: "Active Proposals",
+          url: "#",
+        },
+        {
+          title: "Archived",
+          url: "#",
+        },
+      ],
+    },
+    {
+      title: "Proposal",
+      icon: IconFileDescription,
+      url: "#",
+      items: [
+        {
+          title: "Active Proposals",
+          url: "#",
+        },
+        {
+          title: "Archived",
+          url: "#",
+        },
+      ],
+    },
+    {
+      title: "Prompts",
+      icon: IconFileAi,
+      url: "#",
+      items: [
+        {
+          title: "Active Proposals",
+          url: "#",
+        },
+        {
+          title: "Archived",
+          url: "#",
+        },
+      ],
+    },
+  ],
+  navSecondary: [
+    {
+      title: "Settings",
+      url: "#",
+      icon: IconSettings,
+    },
+  ],
+  documents: [
+    {
+      name: "Reports",
+      url: "#",
+      icon: IconReport,
+    },
+  ],
+}
+
+export function AppSidebar({
+  ...props
+}) {
+  return (
+    <Sidebar collapsible="offcanvas" {...props}>
+      <SidebarHeader>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton asChild className="data-[slot=sidebar-menu-button]:!p-1.5">
+              <a href="#">
+                <IconCreditCard className="!size-5" />
+                <span className="text-base font-semibold">Smart ERP & POS</span>
+              </a>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarHeader>
+      <SidebarContent>
+        <NavMain items={data.navMain} />
+        <NavDocuments items={data.documents} />
+        <NavSecondary items={data.navSecondary} className="mt-auto" />
+      </SidebarContent>
+      <SidebarFooter>
+        <NavUser user={data.user} />
+      </SidebarFooter>
+    </Sidebar>
+  );
+}
---
+++ frontend/components/nav-documents.jsx
@@ -0,0 +1,74 @@
+"use client"
+
+import { IconCalendarClock, IconCalendarMonth, IconCalendarWeek, IconDots } from "@tabler/icons-react";
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import {
+  SidebarGroup,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuAction,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  useSidebar,
+} from "@/components/ui/sidebar"
+
+export function NavDocuments({
+  items
+}) {
+  const { isMobile } = useSidebar()
+
+  return (
+    <SidebarGroup className="group-data-[collapsible=icon]:hidden">
+      <SidebarGroupLabel>Reports</SidebarGroupLabel>
+      <SidebarMenu>
+        {items.map((item) => (
+          <SidebarMenuItem key={item.name}>
+            <SidebarMenuButton asChild>
+              <a href={item.url}>
+                <item.icon />
+                <span>{item.name}</span>
+              </a>
+            </SidebarMenuButton>
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <SidebarMenuAction showOnHover className="data-[state=open]:bg-accent rounded-sm">
+                  <IconDots />
+                  <span className="sr-only">More</span>
+                </SidebarMenuAction>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent
+                className="w-36 rounded-lg"
+                side={isMobile ? "bottom" : "right"}
+                align={isMobile ? "end" : "start"}>
+                <DropdownMenuItem>
+                  <IconCalendarClock />
+                  <span>Today Sales</span>
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                  <IconCalendarWeek />
+                  <span>Weekly Sales</span>
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                  <IconCalendarMonth />
+                  <span>Monthly Sales</span>
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </SidebarMenuItem>
+        ))}
+        {/* <SidebarMenuItem>
+          <SidebarMenuButton className="text-sidebar-foreground/70">
+            <IconDots className="text-sidebar-foreground/70" />
+            <span>More</span>
+          </SidebarMenuButton>
+        </SidebarMenuItem> */}
+      </SidebarMenu>
+    </SidebarGroup>
+  );
+}
